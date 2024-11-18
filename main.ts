930057c2bc83a14426a356cd20a997a8d8f52d60
Darko Pejakovic <pejakovic@gmail.com>
2024-11-18 10:20:35 +0100

    Initial creation of a KaTex to LaTex Obsidian plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin , PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Katex2LatexConverterSettings {
+  enableDefaultPasteConversion: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Katex2LatexConverterSettings = {
+  enableDefaultPasteConversion: false,
+};
+
+export default class Katex2LatexConverterPlugin extends Plugin {
+  settings: Katex2LatexConverterSettings;
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  async onload() {
+    // Load plugin settings
+    await this.loadSettings();
+
+    // Add settings tab
+    this.addSettingTab(new LatexConverterSettingTab(this.app, this));
+
+    // Event for default paste based on the settings 'enableDefaultPasteConversion' value
+    this.registerEvent(
+      this.app.workspace.on("editor-paste", async (evt, editor) => {
+        const clipboardText = evt.clipboardData?.getData("text") || ""
+        const convertedText = this.settings.enableDefaultPasteConversion
+          ? convertKatexToLatex(clipboardText)
+          : clipboardText;
+
+          if (clipboardText) {
+            evt.preventDefault();
+            editor.replaceSelection(convertedText);
+          }
+        
+      })
+    );
+
+    // Command: Paste KaTeX text to LaTeX format
+    this.addCommand({
+      id: "paste-katex-to-latex",
+      name: "Paste with Conversion",
+      editorCallback: (editor) => {
+        navigator.clipboard.readText().then((clipboardText) => {
+          const latexText = convertKatexToLatex(clipboardText);
+          editor.replaceSelection(latexText);
+        });
+      },
+    });
+
+    // Command: Convert existing text in the editor from KaTex to Latex format
+    this.addCommand({
+      id: "convert-editor-text-from-katex-to-latex",
+      name: "Convert Editor Text",
+      editorCallback: (editor) => {
+        const currentText = editor.getValue();
+        const convertedText = convertKatexToLatex(currentText);
+        editor.setValue(convertedText);
+      },
+    });
+
+    // Command: Convert all files in the vault from KaTex to Latex format
+    this.addCommand({
+      id: "convert-all-files-from-katex-to-latex",
+      name: "Convert All Files",
+      callback: async () => {
+        const files = this.app.vault.getMarkdownFiles();
+        for (const file of files) {
+          const content = await this.app.vault.read(file);
+          const convertedContent = convertKatexToLatex(content);
+          await this.app.vault.modify(file, convertedContent);
+        }
+        new Notice("Text in the whole vault is converted from KaTeX to LaTeX!");
+      },
+    });
+
+    console.log("Katex2LatexConverterPlugin loaded!");
+  }
+
+  onunload() {
+    console.log("Katex2LatexConverterPlugin unloaded!");
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+// Helper Function to convert KaTex to LaTeX
+function convertKatexToLatex(input: string): string {
+  return input
+    .replace(/\\\(\s?/g, "$") // Replace `\( ` with `$`
+    .replace(/\s?\\\)/g, "$") // Replace ` \)` with `$`
+    .replace(/\\\[\s?/g, "$$$") // Replace `\[ ` with `$$`
+    .replace(/\s?\\\]/g, "$$$"); // Replace ` \]` with `$$`
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+// Settings tab
+class LatexConverterSettingTab extends PluginSettingTab {
+  plugin: Katex2LatexConverterPlugin;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  constructor(app: App, plugin: Katex2LatexConverterPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  display(): void {
+    const { containerEl } = this;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    containerEl.empty();
 
-	display(): void {
-		const {containerEl} = this;
+    containerEl.createEl("h2", { text: "Latex Converter Settings" });
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    // Toggle for enabling/disabling default paste conversion
+    new Setting(containerEl)
+      .setName("Enable Default Paste Conversion")
+      .setDesc("Automatically converts KaTeX to LaTeX on paste action.")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.enableDefaultPasteConversion)
+          .onChange(async (value) => {
+            this.plugin.settings.enableDefaultPasteConversion = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
